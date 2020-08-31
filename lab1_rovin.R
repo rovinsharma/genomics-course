75d6f16d002d8c8dce22d575b81f78a3404ae58b
Rovin Sharma <rovinsharma@umass.edu>
2020-08-31 10:07:21 -0400

    Added lab 1 files again

---
+++ lab1_rovin.R
@@ -0,0 +1,22 @@
+###Exercise 1
+x <- 3
+y <- 15
+x + y
+x - y
+x * y
+x / y
+
+###Exercise 2
+x <- 2
+y <- 3
+x^5 > y^4
+
+###Exercise 3
+x <- c(22, 62, 148, 43, 129)
+y <- sum (x)
+y
+x / y
+
+###Exercise 4
+Nucleotides <- c("A", "T", "C", "G")
+sort (Nucleotides)
